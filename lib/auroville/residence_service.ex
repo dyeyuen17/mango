893e7caec0d3c4e68060f6428fb47c36ad3f6ca4
dyey <dyeyuenofficial17@gmail.com>
2018-03-22 18:03:16 +0800

    done with registration part

---
+++ lib/auroville/residence_service.ex
@@ -0,0 +1,5 @@
+defmodule Auroville.ResidenceService do
+  def list_areas do
+    ["Ayala", "Eastwood", "BGCD"]
+  end
+end
--- test/mango_web/acceptance/registration_test.exs
+++ test/mango_web/acceptance/registration_test.exs
@@ -5,36 +5,41 @@ defmodule MangoWeb.Acceptance.RegistrationTest do
   hound_session()
 
   test "registers an account with valid data" do
-    navigate_to("/register")
+      navigate_to("/register")
 
-    form = find_element(:id, "registration-form")
-    find_within_element(form, :name, "registration[name]") |> fill_field("John")
+      form = find_element(:id, "registration-form")
+      find_within_element(form, :name, "registration[name]")
+      |> fill_field("John")
 
-    find_within_element(form, :name, "registration[email]") |> fill_field("john@yahoo.com")
+      find_within_element(form, :name, "registration[email]")
+      |> fill_field("john@example.com")
 
-    find_within_element(form, :name, "registration[phone]") |> fill_field("1111")
+      find_within_element(form, :name, "registration[phone]")
+      |> fill_field("1111")
 
-    find_within_element(form, :name, "registration[residence_area]") |> fill_field("Area 1")
+      find_element(:css, "#registration_residence_area option[value='BGCD']") |> click
 
-    find_within_element(form, :name, "registration[password_hash]") |> fill_field("qwerty")
+      find_within_element(form, :name, "registration[password_hash]")
+      |> fill_field("password")
 
-    find_within_element(form, :tag, "button") |> click
+      find_within_element(form, :tag, "button")
+      |> click
 
-    assert current_path == "/"
-    message = find_element(:class, "alert") |> visible_text()
+      assert current_path() == "/"
+      message = find_element(:class, "alert") |> visible_text()
 
-    assert message == "Registration successful"
-  end
+      assert message == "Registration successful"
+    end
 
-  test "shows error messages on invalid data" do
-    navigate_to("/register")
+    test "shows error messages on invalid data" do
+      navigate_to("/register")
 
-    form = find_element(:id, "registration-form")
-    find_within_element(form, :tag, "button") |> click
+      form = find_element(:id, "registration-form")
+      find_within_element(form, :tag, "button") |> click
 
-    assert current_path() == "/register"
-    message = find_element(:id, "form-error") |> visible_text()
-    assert message == "Oops, something went wrong! Please check the errors below."
-  end
+      assert current_path() == "/register"
+      message = find_element(:id, "form-error") |> visible_text()
+      assert message == "Oops, something went wrong! Please check the errors below."
+    end
 
 end
