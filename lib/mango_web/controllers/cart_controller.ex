c81f314e0b821667943eddd2b4d7a762d8ed1f1e
dyey <dyeyuenofficial17@gmail.com>
2018-03-23 17:17:59 +0800

    cart features

---
+++ lib/mango_web/controllers/cart_controller.ex
@@ -0,0 +1,27 @@
+defmodule MangoWeb.CartController do
+  use MangoWeb, :controller
+  alias Mango.Sales
+
+  def add(conn, %{"cart" => cart_params}) do
+    cart = conn.assigns.cart
+    case Sales.add_to_cart(cart, cart_params) do
+      {:ok, _} ->
+        %{"product_name" => name, "pack_size" => size, "quantity" => qty} = cart_params
+        message = "Pruduct added to cart - #{name}(#{size}) x #{qty} qty"
+        conn
+          |> put_flash(:info, message)
+          |> redirect(to: page_path(conn, :index))
+
+      {:error, _} ->
+        conn
+          |> put_flash(:error, "Error adding product to cart")
+          |> redirect(to: page_path(conn, :index))
+    end
+  end
+
+  def show(conn, _params) do
+    cart = conn.assigns.cart
+    render(conn, "show.html", cart: cart)
+  end
+
+end
